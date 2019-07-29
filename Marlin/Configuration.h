130df4f7bb47efe22a88564809a2046b2eed9dac
Stan Smiley <sws777@gmail.com>
2019-07-28 21:45:57 -0600

    Update custom machine name

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -151,7 +151,7 @@
 
 // Name displayed in the LCD "Ready" message and Info menu
 //#define CUSTOM_MACHINE_NAME "ANYCUBIC Kossel"
-#define CUSTOM_MACHINE_NAME "Kossel VSC"
+#define CUSTOM_MACHINE_NAME "Kossel VSC1"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like http://www.uuidgenerator.net/version4
