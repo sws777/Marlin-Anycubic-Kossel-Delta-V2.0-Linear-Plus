f631688983ea0ae2a96291e393f702e172f5dccb
Stan Smiley <sws777@gmail.com>
2019-07-30 06:51:00 -0600

    Enabled BABYSTEP_ZPROBE_OFFSET

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -151,7 +151,7 @@
 
 // Name displayed in the LCD "Ready" message and Info menu
 //#define CUSTOM_MACHINE_NAME "ANYCUBIC Kossel"
-#define CUSTOM_MACHINE_NAME "Kossel VSC1"
+#define CUSTOM_MACHINE_NAME "Kossel VSC2"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like http://www.uuidgenerator.net/version4
